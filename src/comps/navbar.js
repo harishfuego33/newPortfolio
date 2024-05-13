c13906269a6db7b9dba7707b88ecce9b18b5a204
harishfuego33 <harishkumar.ct20@bitsathy.ac.in>
2024-05-13 19:36:04 +0530

    hamburger menu upgraded

--- src/comps/navbar.js
+++ src/comps/navbar.js
@@ -3,36 +3,84 @@ import { useState } from "react";
 function NavBar() {
   const [active, setActive] = useState(false);
   return (
-    <nav className=" flex align-center navbar-box">
-      <div className=" flex align-center ff-inter">
-        <span className="lessthan">&lt;</span>
-        <h1 className="heading">HarisH</h1>
-        <span className="lessthan">&#47;&gt;</span>
-      </div>
-      <ul className="navbar__options flex">
-        <li className="navbar__option">
-          Start<span>&#47;&gt;</span>
+    <>
+      <nav className=" flex align-center navbar-box">
+        <div className=" flex align-center ff-inter">
+          <span className="lessthan">&lt;</span>
+          <h1 className="heading">HarisH</h1>
+          <span className="lessthan">&#47;&gt;</span>
+        </div>
+        <ul className="navbar__options ff-Robot flex">
+          <li className="navbar__option">
+            Start<span>&#47;&gt;</span>
+          </li>
+          <li className="navbar__option">
+            Project<span>&#47;&gt;</span>
+          </li>
+          <li className="navbar__option">
+            About<span>&#47;&gt;</span>
+          </li>
+          <li className="navbar__option">
+            Contact<span>&#47;&gt;</span>
+          </li>
+        </ul>
+        <button
+          className={`hamburger-btn ${active ? "active" : ""}`}
+          onClick={() => setActive(!active)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+        <div className={`background-main ${active ? "active" : ""}`}></div>
+      </nav>
+      <ul className="navbar__center ff-Robot">
+        <li
+          className={`navbar__center-option ${
+            active ? "active" : ""
+          } flex align-center justify-center `}
+          onClick={() => setActive(!active)}
+        >
+          <a href="#Start" className="navbar__link">
+            {" "}
+            Start<span className="lessthan">&#47;&gt;</span>{" "}
+          </a>
         </li>
-        <li className="navbar__option">
-          Project<span>&#47;&gt;</span>
+        <li
+          className={`navbar__center-option ${
+            active ? "active" : ""
+          } flex align-center justify-center `}
+          onClick={() => setActive(!active)}
+        >
+          <a href="#Project" className="navbar__link">
+            {" "}
+            Project<span className="lessthan">&#47;&gt;</span>{" "}
+          </a>
         </li>
-        <li className="navbar__option">
-          About<span>&#47;&gt;</span>
+        <li
+          className={`navbar__center-option ${
+            active ? "active" : ""
+          } flex align-center justify-center `}
+          onClick={() => setActive(!active)}
+        >
+          <a href="#About" className="navbar__link">
+            {" "}
+            About<span className="lessthan">&#47;&gt;</span>{" "}
+          </a>
         </li>
-        <li className="navbar__option">
-          Contact<span>&#47;&gt;</span>
+        <li
+          className={`navbar__center-option ${
+            active ? "active" : ""
+          } flex align-center justify-center `}
+          onClick={() => setActive(!active)}
+        >
+          <a href="#Contact" className="navbar__link">
+            {" "}
+            Contact<span className="lessthan">&#47;&gt;</span>{" "}
+          </a>
         </li>
       </ul>
-      <button
-        className={`hamburger-btn ${active ? "active" : ""}`}
-        onClick={() => setActive(!active)}
-      >
-        <span></span>
-        <span></span>
-        <span></span>
-      </button>
-      <div className={`background-main ${active ? "active" : ""}`}></div>
-    </nav>
+    </>
   );
 }
 export default NavBar;
